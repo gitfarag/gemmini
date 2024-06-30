0cc0af2faa372e6aa080abbd4fd7aa31723bb126
afarag <afarag@easyapplyedu.com>
2024-06-30 12:04:00 +0300

    feat/change-card

--- src/components/UserCard.vue
+++ src/components/UserCard.vue
@@ -1,25 +1,31 @@
 <template>
-  <v-container class="h-screen d-flex flex-column align-start justify-center fluid">
-    <div class="text-h2">Users List</div>
-    <v-row class="mt-6 flex-row align-center justify-center">
+  <v-container
+    class="h-screen d-flex flex-column align-start justify-center fluid"
+  >
+    <div class="text-h2 mt-4 font-weight-bold text-uppercase text-subtitle-1">Users List</div>
+    <v-row class="flex-row align-center justify-center text-light-50">
       <v-col cols="12" sm="6" md="4" v-for="(user, i) in users" :key="i">
-        <v-card color="#1F7087">
-          <div class="d-flex flex-no-wrap">
-            <div>
-              <v-card-title class="text-h5">
-                {{ `${user.first_name} ${user.last_name}` }}
-              </v-card-title>
-              <v-card-subtitle>{{ user.email }}</v-card-subtitle>
-              <v-card-subtitle>USER ID: {{ user.id }}</v-card-subtitle>
-            </div>
-            <v-avatar class="ma-3" rounded="0" size="125">
-              <v-img :src="user.avatar"></v-img>
-            </v-avatar>
-          </div>
+        <v-card class="pb-3">
+          <v-img
+            height="200px"
+            :src="user.avatar"
+            cover
+          ></v-img>
+
+          <v-card-title>
+            {{ `${user.first_name} ${user.last_name}` }}</v-card-title
+          >
+
+          <v-card-subtitle> {{ user.email }}</v-card-subtitle>
         </v-card>
       </v-col>
     </v-row>
-    <v-pagination v-model="page" :length="length" class="my-4" :total-visible="2"></v-pagination>
+    <v-pagination
+      v-model="page"
+      :length="length"
+      class="my-4"
+      :total-visible="2"
+    ></v-pagination>
   </v-container>
 </template>
 
@@ -43,11 +49,11 @@ export default {
   },
   watch: {
     async page() {
-      await this.fetchData({page: this.page});
+      await this.fetchData({ page: this.page });
     },
   },
   async mounted() {
-    await this.fetchData({page: this.page});
+    await this.fetchData({ page: this.page });
   },
 };
 //
